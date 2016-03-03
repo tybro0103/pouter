d7eab1bc7463165c57b9e68fae3ae4285ba8ce12
Tyler Brown <tyler@tmatthew.net>
2016-03-03 11:19:27 -0500

    playing with testing tools

--- tests/utils.js
+++ tests/utils.js
@@ -3,7 +3,9 @@ import * as utils from '../src/router';
 describe('utils', function() {
 
   describe('parseUrl', function() {
-    it('should be tested');
+    it('should be tested', function() {
+      intentional.error();
+    });
   });
 
 });
